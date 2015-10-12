41e25061a08739eb1fabbb68f7978acc7f1167fd
bardolos <whisperdraw@gmail.com>
2015-10-12 11:25:09 +0100

    Final class push

--- tut3/crypto.py
+++ tut3/crypto.py
@@ -3,6 +3,9 @@ import click
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
+modes_ls = dict(
+    CBC = modes.CBC
+)
 
 def decrypt_RC4(connection, outfile):
     key = open("key.txt", 'rb').read(16)
@@ -22,8 +25,11 @@ def decrypt_RC4(connection, outfile):
 
     click.echo(click.style('Decryption successful!', bold = True, fg = 'green'))
 
-def decrypt_AES(connection, outfile):
+def decrypt_AES(connection, outfile, mode_name='CBC'):
     key = open("key.txt", 'rb').read(16)
+    iv  = open("iv.txt", 'rb').read(16)
+
+    mode_ = modes_ls[mode_name](iv)
 
     cipher = Cipher(algorithms.ARC4(key), None, backend=default_backend())
     decryptor = cipher.decryptor()
