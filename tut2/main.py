28736194e74a7d310a94b7774c335bc058ae7fa5
Diogo Martins <nunodiogomartins@gmail.com>
2015-11-08 19:50:06 +0000

    Added KeyAlgorithm, KeyRC4 and KeyAES classes. Created tut2 folder

---
+++ tut2/main.py
@@ -0,0 +1,66 @@
+import os
+import click
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+
+@click.group()
+def cli():
+    """Network Security - Diogo Martins & João Meira"""
+
+@cli.command(short_help = 'Generate key')
+@click.argument('keyfile', type = click.File('wb'))
+def genkey(keyfile):
+    """Generates key for the encryption"""
+    key = os.urandom(16)
+    keyfile.write(key)
+
+    click.echo(click.style('Keygen successful!', bold = True, fg = 'green'))
+
+@cli.command(short_help = 'Encrypt file')
+@click.argument('keyfile', type = click.File('rb'))
+@click.argument('infile', type = click.File('rb'))
+@click.argument('outfile', type = click.File('wb'))
+def enc(keyfile, infile, outfile):
+    """Encrypts file with the given key"""
+    key = keyfile.read(16)
+
+    cipher = Cipher(algorithms.ARC4(key), mode=None, backend=default_backend())
+    encryptor = cipher.encryptor()
+
+    pt = b""
+    while True:
+        chunk = infile.read(1)
+        if not chunk:
+            break
+        pt += chunk
+
+    ct = encryptor.update(pt)
+    outfile.write(ct)
+
+    click.echo(click.style('Encryption successful!', bold = True, fg = 'green'))
+
+@cli.command(short_help = 'Decrypt file')
+@click.argument('keyfile', type = click.File('rb'))
+@click.argument('infile', type = click.File('rb'))
+@click.argument('outfile', type = click.File('wb'))
+def dec(keyfile, infile, outfile):
+    """Decrypts file with the given key"""
+    key = keyfile.read(16)
+
+    cipher = Cipher(algorithms.ARC4(key), mode=None, backend=default_backend())
+    decryptor = cipher.decryptor()
+
+    ct = b""
+    while True:
+        chunk = infile.read(1)
+        if not chunk:
+            break
+        ct += chunk
+
+    dt = decryptor.update(ct)
+    outfile.write(dt)
+
+    click.echo(click.style('Decryption successful!', bold = True, fg = 'green'))
+
+if __name__ == "__main__":
+    cli()
---
+++ tut3/ciphers.py
@@ -0,0 +1,57 @@
+import os, sys
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import padding
+
+modes_list = {'CBC': modes.CBC, 'CFB': modes.CFB, 'CFB8': modes.CFB8}
+
+class KeyAlgorithm:
+    def encrypt(self, text):
+        enc = self.cipher.encryptor()
+        ct = enc.update(text) + enc.finalize()
+        return ct
+
+    def decrypt(self, text):
+        dec = self.cipher.decryptor()
+        pt = dec.update(text) + dec.finalize()
+        return pt
+
+
+class KeyRC4(KeyAlgorithm):
+    def __init__(self, key):
+        self.algorithm = algorithms.ARC4(key)
+        self.mode = None
+        self.backend = default_backend()
+
+        self.cipher = Cipher(self.algorithm, self.mode, self.backend)
+
+class KeyAES(KeyAlgorithm):
+    def __init__(self, key, mode):
+        self.iv = os.urandom(16)
+        self.algorithm = algorithms.AES(key)
+        self.mode = modes_list[mode](self.iv)
+        self.backend = default_backend()
+
+        self.cipher = Cipher(self.algorithm, self.mode, self.backend)
+
+    def encrypt(self, text):
+        text = self.padding(text)
+        ct = super(KeyAES, self).encrypt(text)
+        return ct
+
+    def decrypt(self, text):
+        pt = super(KeyAES, self).decrypt(text)
+        pt = self.unpadding(pt)
+        return pt
+
+    def padding(self, text):
+        padder = padding.PKCS7(128).padder()
+        padded_data = padder.update(text)
+        padded_data += padder.finalize()
+        return padded_data
+
+    def unpadding(self, text):
+        unpadder = padding.PKCS7(128).unpadder()
+        data = unpadder.update(text)
+        data += unpadder.finalize()
+        return data
