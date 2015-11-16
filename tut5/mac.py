248dca1061c992091a98311fb3f4a83a34100e04
Diogo Martins <nunodiogomartins@gmail.com>
2015-11-16 11:30:59 +0000

    tut5 working

--- tut5/mac.py
+++ tut5/mac.py
@@ -1,22 +1,28 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, hmac
+import os
 
 class MAC:
     def __init__(self, key):
         self.mac = hmac.HMAC(key, hashes.SHA256(), backend=default_backend())
         self.counter = 0
 
-    def genMAC(self, text, counter):
+    def genMAC(self, text, counter = self.counter):
+        self.counter = counter
         if type(text) == str:
             text = bytes(text, encoding = 'UTF-8')
 
         self.mac.update(text)
-        self.mac.update(bytes(counter))
+        self.mac.update(bytes(str(counter), encoding = 'UTF-8'))
         return self.mac.finalize()
 
-    def verMAC(self, text, counter, rcv_mac):
+    def verMAC(self, text, rcv_mac, counter = self.counter):
+        self.counter = counter
         mac = self.genMAC(text, counter)
         if mac == rcv_mac:
             return True
         else:
             return False
+
+    def counterUp(self):
+        self.counter += 1
