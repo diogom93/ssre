951ac8476d6d87c85943083d2e116c3e02057b07
bardolos <whisperdraw@gmail.com>
2015-11-16 11:08:06 +0000

    sync client

--- tut5/client.py
+++ tut5/client.py
@@ -19,13 +19,6 @@ def cli(filename):
     except socket.error as msg:
         click.echo(click.style('Error connecting to server: ' + str(msg[1]), bold = True, fg = 'red'))
 
-    pt = b""
-    while True:
-        chunk = filename.read(100)
-        if not chunk:
-            break
-        pt += chunk
-
     ks = KeyStore('enc_key.store', os.path.abspath(''))
     key = str.encode(ks.keys['mother_base_key'].public_key)
 
@@ -33,29 +26,36 @@ def cli(filename):
     sk1 = sk[0:16]
     sk2 = sk[16:32]
 
-    so = SealedObject()
     cipher = ciphers.KeyAES(key, 'CFB8', False)
-
-    mac_ = mac.MAC(sk2)
-
     ct = cipher.encrypt(sk)
-    hmac = mac_.genMAC(ct, 0)
-    iv = cipher.iv
 
-    pkt = packet.Packet(ct, None, iv)
+    pkt = packet.Packet(ct, None, cipher.iv)
+
+    so = SealedObject()
     csk = so.serialize(pkt)
-    s.sendall(csk)
 
+    s.sendall(csk)
     s.recv(512)
 
+    pt = b""
+    while True:
+        chunk = filename.read(100)
+        if not chunk:
+            break
+        pt += chunk
 
     cipher = ciphers.KeyAES(sk1, 'CFB8', False)
     ct = cipher.encrypt(pt)
-    
-    s.send(ct)
 
-    cipher = Cipher(algorithms.AES(sk1), mode=modes.CFB8(open('iv.txt', 'rb').read(16)), backend=default_backend())
-    encryptor = cipher.encryptor()
+    hmac = mac.MAC(sk2)
+    mac = hmac.genMAC(ct, 0)
+
+    pkt = packet.Packet(ct, None, cipher.iv)
+
+    so = SealedObject()
+    csk = so.serialize(pkt)
+
+    s.send(csk)
 
     s.close()
 
